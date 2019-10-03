0f29cb6b0706450f8e49278082d69759ffec81a1
Joren Six <joren.six@ugent.be>
2019-10-03 15:59:48 +0200

    Sample rate fix and html UI update

--- scripts/the_midimorphosis.rb
+++ scripts/the_midimorphosis.rb
@@ -113,45 +113,64 @@ midi_files.each do |midi_file|
 
 	previous_time_in_seconds=0
 
+	#assumes that midi CC messages are sent at the same time 
+	#
+	timestamp_stored = false
+	lines = Array.new
+	line = Array.new
+	first = true 
+	data_hash = Hash.new
+	prev_data_hash = Hash.new
+
 	midi_csv.split("\n").reject{|l| l !~ /.+Control_c.+/}.each do |midi_event|
 		midi_msg_data = midi_event.split(",")
 		time_in_midi_clocks = midi_msg_data[1].to_i
-		time_in_seconds = 1/1000000.0 * time_in_midi_clocks / clock_pulses_per_quarter_note * tempo
-
-		unless data_hash[time_in_seconds]
-			data_hash[time_in_seconds] = Hash.new
-			#initialize each cc value on zero
-			if(previous_time_in_seconds !=0)
-				cc_number_range.each{|cc_number| data_hash[time_in_seconds][cc_number]= data_hash[previous_time_in_seconds][cc_number]  }
-			else
-				cc_number_range.each{|cc_number| data_hash[time_in_seconds][cc_number]= 0 }
-			end
-		end
-
-		previous_time_in_seconds= time_in_seconds
-
+		
 		channel = midi_msg_data[3].to_i
 		cc_number = midi_msg_data[4].to_i
 		cc_value = midi_msg_data[5].to_i
 
-		data_hash[time_in_seconds][cc_number] = cc_value
+		if cc_number = 88
+			#store the current line if there is a current line
+			lines << line if line.size > 0
+
+			#start a new line
+			line = Array.new
+			prev_data_hash = data_hash
+			data_hash = Hash.new
+
+			time_in_seconds = 1/1000000.0 * time_in_midi_clocks / clock_pulses_per_quarter_note * tempo
+			line << time_in_seconds
+			line << data_hash
+
+			#initialize the data_hash, set each key to -1
+			cc_number_range.each{|cc_number| line[1][cc_number] = -1}
+
+			#Base the current values on the previous values
+			prev_data_hash.keys.each{|cc_number| line[1][cc_number] = prev_data_hash[cc_number]}
+		else
+			line[1][cc_number] = cc_value
+		end
 	end
 
 	File.open(csv_file,"w") do |f|
-		data_hash.keys.sort.each do |time_in_seconds|
-			line = Array.new
-			line << "%.6f" % time_in_seconds
+		lines.each do |line|
+			print_line = Array.new
+			print_line[0] = "%.6f" % line[0]
+			data_hash = line[1]
 			cc_number_range.step(2).each do |cc_number|
-				msb = data_hash[time_in_seconds][cc_number]
-				
-				lsb = data_hash[time_in_seconds][cc_number+1]
-				#from 14 bits to 13bits (the resolution of the analog read)
-				value = (msb * (1<<7) + lsb) / 2
-				#line << msb
-				#line << lsb
-				line << value.to_s
+				if(data_hash[cc_number] == -1 or data_hash[cc_number+1] == -1 )
+					puts "ERROR no data for #{cc_number}"
+					print_line << ""
+				else
+
+					msb = data_hash[cc_number]
+					lsb = data_hash[cc_number+1]
+					#from 14 bits to 13bits (the resolution of the analog read)
+					value = (msb * (1<<7) + lsb) / 2
+					print_line << value.to_s
+				end
 			end
-			f.puts line.join(", ")
-		end
+			f.puts print_line.join(", ")
 	end
 end
--- teensy/analog_to_midi_cc/analog_to_midi_cc.ino
+++ teensy/analog_to_midi_cc/analog_to_midi_cc.ino
@@ -2,10 +2,7 @@
 * This program translates analog inputs to MIDI CC messages according to a predefined protocol. 
 * It is tested on a Teensy 3.2
 * Note that the Teensy needs to be programmed as MIDI Device for this to work.
-*
 */
-
-#include <EEPROM.h>
 
 // Configuration settings -----------
 
@@ -13,93 +10,123 @@
 const int ANALOG_READ_INPUTS = 8;
 
 //interrupt pin for triggering
-int interruptPin=A8;
+int interruptPin=A9;
 
-//pin A9 is used for the coupled  board
-
-//print debug information
-boolean debug = false;
-
-// Use an inernal clock or external clock input
-boolean internalClock = false;
+//start taking samples without clock
+boolean autostartStamplerTimer = false;
 
 // End configuration settings ----------
 
+
 //timer for the measurements
-IntervalTimer myTimer;
+IntervalTimer samplerTimer;
 
 //Timer for the heartbeat interval 
 // sends note on and off events every second or so
 IntervalTimer heartBeatTimer;
 
-//is the heartbeat note on or not
-volatile boolean noteIsOn = false;
+//is the heartbeat note currently on or not
+boolean heartBeatnoteIsOn = false;
 
-volatile long lastSend = 0;
+//should we send a heart beat note message?
+volatile boolean sendHeartBeatNow = false;
 
-volatile byte maxWheight=1;
-volatile byte prevNote = 0;
+//Should we measure the analog inputs and send the data?
+volatile boolean sendMeasurementsNow = false;
 
-static const int INTERNAL_CLOCK_PERIOD = 8333; //in microseconds, 1 / (8333 microseconds) = 120.0048 hertz
+volatile boolean interruptInUse = false;
+boolean timerInUse = false;
 
-//map to remap analog inputs to channels
-//e.g A0 to 1 and A1 to 0
-int redirectMap[ANALOG_READ_INPUTS];
+long lastSend = 0;
+
+byte maxWheight=1;
+byte prevNote = 0;
+
+//The midi cc clock makes sure that at least one value changes every measurement sample
+//This enables a stable sampling even when only value changes are saved as is common in MIDI
+const int MIDI_CC_CLOCK_CHANNEL = 15;
+const int MIDI_CC_CLOCK_CONTROLLER_NUMBER = 88;
+
+const int DEFAULT_SAMPLE_PERIOD_IN_MICROS = 8333;//or about 120Hz
+
+const int LED_PIN = 13;
+
+int current_midi_cc_clock_value = 64;
 
 void setup() {
   //increase analog read resolution to 13 bits
   analogReadResolution(13);
+
+  //We want to use the internal LED
+  pinMode(LED_PIN,OUTPUT);
   
-  // Initialize serial port
+  // Initialize serial port for debug messages
   Serial.begin(115200);
 
-  // Initialize redirect map
-  for(int i = 0;i<ANALOG_READ_INPUTS;i++){
-    redirectMap[i] = i;
-  }
-
   //every second send a heartbeat note on/off pulse
-  heartBeatTimer.begin(sendHeartBeat, 1000000);
-  
-  if(internalClock){
-    // Use the configured period and start the timer
-    myTimer.begin(sampleAndSend, INTERNAL_CLOCK_PERIOD);
+  heartBeatTimer.begin(toggleSendHeartBeat, 1000000);
+
+  // Use the rising flank on the interrupt pin
+  attachInterrupt(interruptPin, toggleSampleAndSendFromPin, RISING); // interrrupt 1 is data ready
+
+  if(autostartStamplerTimer){
+    samplerTimer.begin(toggleSampleAndSendFromTimer, DEFAULT_SAMPLE_PERIOD_IN_MICROS);
+    timerInUse = true;
+     Serial.println("Started sampling with internal clock at 120Hz");
   }else{
-    // Use the rising flank on the interrupt pin
-    attachInterrupt(interruptPin, sampleAndSend, RISING); // interrrupt 1 is data ready
+     Serial.println("Waiting for clock input or sample rate setting");
   }
+ 
 }
 
+//short interrupt methods
+void toggleSendHeartBeat(){
+  sendHeartBeatNow = true;
+}
+
+void toggleSampleAndSendFromPin(){
+  sendMeasurementsNow = true;
+  interruptInUse = true;
+}
+
+void toggleSampleAndSendFromTimer(){
+  sendMeasurementsNow = true;
+}
+
+
 void sendHeartBeat(){
+  
   int channel = 15;
   int velocity = millis() - lastSend;
   //limit range to 1-127
   velocity = min(velocity,127);
   velocity = max(1,velocity);
 
-  if(noteIsOn){
+  if(heartBeatnoteIsOn){
     usbMIDI.sendNoteOff(prevNote,0,channel);
-    noteIsOn = false;
+    heartBeatnoteIsOn = false;
   }else{
     usbMIDI.sendNoteOn(maxWheight,70,channel);
-    noteIsOn=true;
+    heartBeatnoteIsOn=true;
     prevNote = maxWheight;
     maxWheight = 1;
   }
+  digitalWrite(LED_PIN,!digitalRead(LED_PIN));
 }
 
-void sampleAndSend(){
-  cli();
-  sendAndSample();
-  lastSend = millis();
-  sei();
-}
 
 void sendAndSample(){
+  //first send a midi cc value that always changes
+  //this can be used to split messages
+  usbMIDI.sendControlChange(MIDI_CC_CLOCK_CONTROLLER_NUMBER, current_midi_cc_clock_value, MIDI_CC_CLOCK_CHANNEL);
+  current_midi_cc_clock_value++;
+  if(current_midi_cc_clock_value == 128){
+    current_midi_cc_clock_value = 0;
+  }
 
   for(int i = 0 ; i < ANALOG_READ_INPUTS ; i++){
     int analogReadIndex = 14+i; // start form port 14
-    int index = redirectMap[i];
+    int index = i;
     int channel = 1 + index; //start from channel 1 
     int cc_controller_msb = 102+index*2; // 102 to 119 controllers are undefined
     int cc_controller_lsb = cc_controller_msb + 1; //least significant 7 bits
@@ -116,6 +143,7 @@ void sendAndSampleSingle(int analogReadIndex,int channel,int cc_controller_msb,i
     int value = analogRead(analogReadIndex); 
     value = value << 1;// 13 bits to 14 bits multiply by two
 
+    // For heartbeat
     byte wheightInByte = value >> 7;//to 7 bits (0-128)
     maxWheight = max(maxWheight,wheightInByte);
 
@@ -129,6 +157,31 @@ void sendAndSampleSingle(int analogReadIndex,int channel,int cc_controller_msb,i
 }
 
 void loop() {
+  // If requested by the pin or timer, 
+  // sample analog pins and send midi data
+  if(sendMeasurementsNow){
+    sendMeasurementsNow = false;
+    sendAndSample();
+    lastSend = millis();
+  }
+
+  // If requested by the timer, 
+  // send a heartbeat pulse
+  if(sendHeartBeatNow){
+    sendHeartBeatNow = false;
+
+    if(interruptInUse || timerInUse){
+      sendHeartBeat();
+    }
+  }
+
+  // If both timer and pin interrupt are in use
+  // give priority to the interrupt and stop the timer
+  if(interruptInUse && timerInUse){
+    samplerTimer.end();
+    timerInUse = false;
+  }
+
   // MIDI Controllers should discard incoming MIDI messages.
   while (usbMIDI.read()) {
     // parse incomming messages
@@ -137,41 +190,25 @@ void loop() {
     //1 = Note On
     //6 = Pitch bend 
     // see here https://www.pjrc.com/teensy/td_midi.html
-    if(type==1) { //0x90
+    if(type==usbMIDI.PitchBend) { 
       setNewSampleRate(usbMIDI.getData1(),usbMIDI.getData2());
-    }else if (type==0) { //0x80
-      setNewMapping(usbMIDI.getData1(),usbMIDI.getData2());
-    } else if (type==6){ //0xE0
-      setTrigger(usbMIDI.getData1(),usbMIDI.getData2());
     }
+    Serial.println(type);
   }
 }
+
 
 void setNewSampleRate(int firstByte,int secondByte){
-    int periodInHz = firstByte * 127 + secondByte;
-    int periodInMicros = round(1.0 / (periodInHz + 0.0) * 1000000.0);
-    if(debug){
-      Serial.print("Period in micros: ");
-      Serial.println(periodInMicros);
-    }
-    myTimer.end();
-    myTimer.begin(sampleAndSend, periodInMicros);
-}
+  int periodInHz = firstByte * 127 + secondByte;
+  int periodInMicros = round(1.0 / (periodInHz + 0.0) * 1000000.0);
 
-void setTrigger(int port,int enable){
-  if(enable==1){
-    attachInterrupt(port, sampleAndSend, RISING);
-  }else{
-    detachInterrupt(port);
+  Serial.print("Period in micros: ");
+  Serial.println(periodInMicros);
+
+  if(timerInUse){
+    samplerTimer.end();
   }
+    
+  samplerTimer.begin(toggleSampleAndSendFromTimer, periodInMicros);
+  timerInUse = true;
 }
-
-void setNewMapping(int index,int redirect){
-  redirectMap[index]=redirect;
-  Serial.print("Redirect ");
-  Serial.print(index);
-  Serial.print(" to: ");
-  Serial.println(redirect);
-  EEPROM.write(index,redirect);
-}
-
